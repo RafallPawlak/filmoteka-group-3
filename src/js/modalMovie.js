0359998e45870ff5be847a13723c6bf7c2a35d06
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-11-20 16:54:09 +0100

    \

--- src/js/modalMovie.js
+++ src/js/modalMovie.js
@@ -1,45 +1,45 @@
-import { fetchApiGetDetailsFilm } from "./fetchMovie";
+// import { fetchApiGetDetailsFilm } from "./fetchMovie";
 
-const overlay = document.querySelector('.modal-overlay');
-const body = document.querySelector('body');
-const movieDescription = document.querySelector('[data-movie-description]');
-const card = document.querySelector('.grid');
-overlay.addEventListener('click', onCloseModal);
-card.addEventListener('click', onOpenModal);
+// const overlay = document.querySelector('.modal-overlay');
+// const body = document.querySelector('body');
+// const movieDescription = document.querySelector('[data-movie-description]');
+// const card = document.querySelector('.grid');
+// overlay.addEventListener('click', onCloseModal);
+// card.addEventListener('click', onOpenModal);
 
-export function onOpenModal() {
-//   const marginSize = window.innerWidth - body.clientWidth;
-//   if (marginSize) {
-//     body.style.marginRight = marginSize + 'px';
+// export function onOpenModal() {
+// //   const marginSize = window.innerWidth - body.clientWidth;
+// //   if (marginSize) {
+// //     body.style.marginRight = marginSize + 'px';
+// //   }
+//     console.log("modal");
+// //   overlay.classList.add('visiable');
+// //   body.classList.add('no-scroll');
+
+// //   window.addEventListener('keydown', onEscClick);
+// }
+
+
+// export function onCloseModal(e) {
+//   if (e.target === overlay) {
+//     closeModal();
 //   }
-    console.log("modal");
-//   overlay.classList.add('visiable');
-//   body.classList.add('no-scroll');
+// }
 
-//   window.addEventListener('keydown', onEscClick);
-}
-
-
-export function onCloseModal(e) {
-  if (e.target === overlay) {
-    closeModal();
-  }
-}
-
-export function closeModal() {
-  overlay.classList.remove('visiable');
-  body.classList.remove('no-scroll');
-  movieDescription.textContent = '';
-  body.style.marginRight = '';
-}
+// export function closeModal() {
+//   overlay.classList.remove('visiable');
+//   body.classList.remove('no-scroll');
+//   movieDescription.textContent = '';
+//   body.style.marginRight = '';
+// }
 
 
-function open(e) {
-  if (e.target.parentElement.className ) {
+// function open(e) {
+//   if (e.target.parentElement.className ) {
      
-    return console.log("modal");
-  }
+//     return console.log("modal");
+//   }
 
-}
+// }
 
-card.addEventListener("click", open);
+// card.addEventListener("click", open);
