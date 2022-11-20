4ced3b17fd156088ddf299d4cf6ed2ddc677d517
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-11-20 11:47:38 +0100

    Update localStorage.js

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -25,35 +25,143 @@ const removeKey = key => {
     }
 };
 
-async function changeWatchedQueueList(event) {
+// changing text in modal buttons depending on their assignment to local storage key
+async function textModalButtons(id) {
+    const watchedButton = document.querySelector(".klasaOpisujacaPrzyciskWatchedWModalu");
+    const queueButton = document.querySelector(".klasaOpisujacaPrzyciskQueueWModalu");
+    // checking if id is in the list
+    function localStorageCheck(id, key) {
+        let array = [];
+        let existingArray = getKey(key);
+        if (existingArray) {
+            array = [...existingArray]
+        }
+        const listSet = new Set(array);
+        return listSet.has(id);
+    }
+    
+    if (!localStorageCheck(id, 'watched')) {
+        watchedButton.textContent = 'Add to watched';
+        watchedButton.classList.remove('in-local-storage');
+    } else {
+        watchedButton.textContent = 'Remove from watched';
+        watchedButton.classList.add('in-local-storage');
+    }
+    
+    if (!localStorageCheck(id, 'queue')) {
+        queueButton.textContent = 'Add to queue';
+        queueButton.classList.remove('in-local-storage');
+    } else {
+        queueButton.textContent = 'Remove from queue';
+        queueButton.classList.add('in-local-storage');
+    }
+}
+
+async function changeWatchedQueueList(id) {
     try {
-        const movieID = await funkcjaWyszukującaFilmPoJegoId(id);
-        funkcjaTworzacaModal(movieID);
+        // const movieID = await funkcjaWyszukującaFilmPoId(id);
+        // funkcjaTworzacaModal(movieID);
 
         const watchedButton = document.querySelector(".klasaOpisujacaPrzyciskWatchedWModalu");
         const queueButton = document.querySelector(".klasaOpisujacaPrzyciskQueueWModalu");
 
-        watchedButton.addEventListener("click",);
-        queueButton.addEventListener("click",);
+        watchedButton.addEventListener("click", addToWatched);
+        queueButton.addEventListener("click", addToQueue);
     } catch (error) {
-    console.log(error);
+        console.log(error);
     }
+    // adding film ID to local storage key "watched"
     function addToWatched() {
-    let watchedList = [];
-    let existingWatchedList = load(watched);
-    watchedList = [...existingWatchedList];
+        const watchedButton = document.querySelector(".klasaOpisujacaPrzyciskWatchedWModalu");
+        if (!watchedButton.classList.contains('in-local-storage')) {
+            removeFromWatched(id);
+        } else {
+            let watchedList = [];
+            let existingWatchedList = getKey('watched');
+            if (existingWatchedList) {
+                watchedList = [...existingWatchedList];
+            }
 
-    let queueList = [];
-    let existingQueueList = load(queue);
-    queueList = [...existingQueueList];
+            let queueList = [];
+            let existingQueueList = getKey('queue');
+            if (existingWatchedList) {
+                queueList = [...existingQueueList];
+            }
+        
+            const watchedSet = new Set(watchedList);
+            if (watchedSet.has(id)) {
+                textModalButtons(id);
+            } else { 
+                watchedList.push(id);
+                saveKey('watched', watchedList);
+                textModalButtons(id);
+            }
+
+            let queueSet = new Set(queueList);
+            if (queueSet.has(id)) {
+                removeKey('queue');
+                let filmIndex = queueList.idIndexOf(id);
+                queueList.splice(filmIndex, 1);
+                saveKey('queue', queueList);
+            }
+        }
     }
+    // adding film ID to local storage key "queue"
+    function addToQueue() {
+        const queueButton = document.querySelector(".klasaOpisujacaPrzyciskQueueWModalu");
+        if (queueButton.classList.contains('active')) {
+            removeFromQueue(id);
+        } else {
+            let queuedList = [];
+            let existingQueueList = getKey('queue');
+            if (existingQueueList) {
+                queuedList = [...existingQueueList];
+            }
+            let watchedList = [];
+            let existingWatchedList = getKey('watched');
+            if (existingWatchedList) {
+                watchedList = [...existingWatchedList];
+            }     
 
-    const watchedSet = new Set(watchedList);
-    watchedList.push(id);
-    if (!watchedSet.has(id)) {
-        saveKey('watched', watchedList)
+            const queueSet = new Set(queueList);
+            queueList.push(id);
+            if (queueSet.has(id)) {
+                saveKey('queue', queueList)
+            }
+
+            let watchedSet = new Set(watchedList);
+            if (watchedSet.has(id)) {
+                removeKey('watched');
+                let filmIndex = watchedList.idIndexOf(id);
+                watchedList.splice(filmIndex, 1);
+                saveKey('watched', watchedList);
+            }
+        }
     }
-
-    let queueSet = new Set(queueList);
-    
+    // removing film ID from local storage key "watched"
+    function removeFromWatched(id) {
+        let watchedList = [];
+        let existingWatchedList = getKey('watched');
+        if (existingWatchedList) {
+            watchedList = [...existingWatchedList];
+        }
+        removeKey('watched');
+        let filmIndex = watchedList.idIndexOf(id);
+        watchedList.splice(filmIndex, 1);
+        saveKey('watched', watchedList);
+        textModalButtons();
+    }
+    // removing film ID from local storage key "queue"
+    function removeFromQueue(id) {
+        let queueList = [];
+        let existingQueueList = getKey('queue');
+        if (existingWatchedList) {
+            queueList = [...existingQueueList];
+        }
+        removeKey('queue');
+        let filmIndex = queueList.idIndexOf(id);
+        queueList.splice(filmIndex, 1);
+        saveKey('queue', queueList);
+        textModalButtons;
+    }
 }
