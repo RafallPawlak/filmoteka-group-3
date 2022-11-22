060a46196a27e6d6c7f5decea3be85bb3a6390d0
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-11-22 20:16:13 +0100

    Update fetchMovie.js

--- src/js/fetchMovie.js
+++ src/js/fetchMovie.js
@@ -284,10 +284,7 @@ export async function createHtmlTags(result){
 
       <div class="description__text-wrapper">
         <p class="description__text">
-          <span class="vote--accent">${roundingMethodToFirstPlace( filmDetails.vote_average) }</span>&#47;<span class="vote">${ filmDetails.vote_count }</span
-          >
-          <span class="vote vote--accent">${ filmDetails.vote_average }&#47;</span>
-          <span class="vote">${ filmDetails.vote_count }</span>
+          <span class="vote--accent">${roundingMethodToFirstPlace( filmDetails.vote_average) }</span>&#47;<span class="vote">${ filmDetails.vote_count }</span>
         </p>
         <p class="description__text description__popularity">${ filmDetails.popularity }</p>
         <p class="description__text">${ filmDetails.original_title}</p>
